c6308334eefb6f3d192dfb06b3b4e7820290640b
HeavenElite <heaven.elite@gmail.com>
2022-10-17 17:48:37 +0800

    Updated

--- SoftwareCheckRemoteScript.ps1
+++ SoftwareCheckRemoteScript.ps1
@@ -15,20 +15,64 @@ if ($Env:PROCESSOR_ARCHITECTURE -match '86') {
         Version      = $Response.DisplayVersion
     }
 
+    [PSCustomObject]$Result
+
 }
 elseif ($Env:PROCESSOR_ARCHITECTURE -match '64') {
 
     $Response = Get-ChildItem -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\' | Get-ItemProperty | Select-Object -Property DisplayName,DisplayVersion | Where-Object {$_.Displayname -like "*$Software*"}
-    $Result = @{
-        Computer     = $IPAddress
-        Processor    = $Env:PROCESSOR_ARCHITECTURE
-        OSName       = [System.Environment]::OSVersion.VersionString
-        OSArck       = (wmic os get osarchitecture)[2]
-        Software     = $Response.DisplayName
-        SoftArck     = '64-Bit'
-        Version      = $Response.DisplayVersion
+    
+    if ($null -ne $Response) {
+
+        $Result = @{
+            Computer     = $IPAddress
+            Processor    = $Env:PROCESSOR_ARCHITECTURE
+            OSName       = [System.Environment]::OSVersion.VersionString
+            OSArck       = (wmic os get osarchitecture)[2]
+            Software     = $Response.DisplayName
+            SoftArck     = '64-Bit'
+            Version      = $Response.DisplayVersion
+        }
+
+        [PSCustomObject]$Result
+    }
+
+    else {
+
+        $Response = Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\' | Get-ItemProperty | Select-Object -Property DisplayName,DisplayVersion | Where-Object {$_.Displayname -like "*$Software*"}
+
+        if ($null -ne $Response) {
+
+            $Result = @{
+                Computer     = $IPAddress
+                Processor    = $Env:PROCESSOR_ARCHITECTURE
+                OSName       = [System.Environment]::OSVersion.VersionString
+                OSArck       = (wmic os get osarchitecture)[2]
+                Software     = $Response.DisplayName
+                SoftArck     = '32-Bit'
+                Version      = $Response.DisplayVersion
+            }
+            
+            [PSCustomObject]$Result
+        }
+
+        else {
+
+            $Result = @{
+                Computer     = $IPAddress
+                Processor    = $Env:PROCESSOR_ARCHITECTURE
+                OSName       = [System.Environment]::OSVersion.VersionString
+                OSArck       = (wmic os get osarchitecture)[2]
+                Software     = "$SoftwareNotInstalled"
+                SoftArck     = "$SoftwareNotInstalled"
+                Version      = "$SoftwareNotInstalled"
+            }
+
+            [PSCustomObject]$Result
+        }
     }
 }
+
 else {
 
     $Result = @{
@@ -36,10 +80,10 @@ else {
         Processor    = $Env:PROCESSOR_ARCHITECTURE
         OSName       = [System.Environment]::OSVersion.VersionString
         OSArck       = (wmic os get osarchitecture)[2]
-        Software     = 'N/A'
-        SoftArck     = 'N/A'
-        Version      = 'N/A'
+        Software     = 'ProcessorInfoError'
+        SoftArck     = 'ProcessorInfoError'
+        Version      = 'ProcessorInfoError'
     }
-}
 
-[PSCustomObject]$Result
+    [PSCustomObject]$Result
+}
