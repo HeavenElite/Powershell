8ed4171da048f3dd5cc5a8fb53f79c07f68db0dd
HeavenElite <heaven.elite@gmail.com>
2022-10-19 18:51:55 +0800

    Updated

--- RDPCheck.ps1
+++ RDPCheck.ps1
@@ -1,25 +1,52 @@
 $Computers = Import-Csv .\ITLabData.csv
+
 for ($i=0; $i -lt $Computers.Length; $i++) {
 
     $Username   = $Computers[$i].Account
     $Password   = ConvertTo-SecureString -AsPlainText $Computers[$i].Password -Force
     $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
-    $Data = Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational'} | Where-Object {$_.ID -eq '1149' -and $_.TimeCreated -like "*$(Get-Date -Format 'MM/dd/yyyy')*"}
+    $LoopContinue = $true
 
-    for ($m=0; $m -lt $Data.Length; $m++) {
-    
+    try {
+
+        $Data = Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational'} | Where-Object {$_.ID -eq '1149' -and $_.TimeCreated -like "*$(Get-Date -Format 'MM/dd/yyyy')*"}
+    }
+    catch {
+
         $Report = @{
-    
-            User      = ($Data[$m].Message | Select-String -Pattern '地址:.*').Matches.Value -replace '地址: ',''
+
+            User      = 'PCOffline'
             Device    = $Computers[$i].IP
-            Account   = ($Data[$m].Message | Select-String -Pattern '用户: .*').Matches.Value -replace '用户: ','' -replace "`r",''
-            Domain    = ($Data[$m].Message | Select-String -Pattern '域: .*').Matches.Value -replace '域: ','' -replace "`r",''
-            LoginTime = $Data[$m].TimeCreated
-    
+            Account   = 'PCOffline'
+            Domain    = 'PCOffline'
+            LoginTime = 'PCOffline'  
         }
+
         [PSCustomObject]$Report | Select-Object -Property User,Device,Account,Domain,LoginTime | Format-Table -AutoSize
         [PSCustomObject]$Report | Select-Object -Property User,Device,Account,Domain,LoginTime | Export-Csv -Path "D:\Desktop\Powershell\RDPReport$(Get-Date -Format 'yyyy.MM.dd').csv" -Append -NoTypeInformation
 
+        $LoopContinue = $false 
+    }    
+    finally {
+        
+    if ($LoopContinue) {
+
+        for ($m=0; $m -lt $Data.Length; $m++) {
+        
+            $Report = @{
+        
+                User      = ($Data[$m].Message | Select-String -Pattern '地址:.*').Matches.Value -replace '地址: ',''
+                Device    = $Computers[$i].IP
+                Account   = ($Data[$m].Message | Select-String -Pattern '用户: .*').Matches.Value -replace '用户: ','' -replace "`r",''
+                Domain    = ($Data[$m].Message | Select-String -Pattern '域: .*').Matches.Value -replace '域: ','' -replace "`r",''
+                LoginTime = $Data[$m].TimeCreated
+        
+            }
+            [PSCustomObject]$Report | Select-Object -Property User,Device,Account,Domain,LoginTime | Format-Table -AutoSize
+            [PSCustomObject]$Report | Select-Object -Property User,Device,Account,Domain,LoginTime | Export-Csv -Path "D:\Desktop\Powershell\RDPReport$(Get-Date -Format 'yyyy.MM.dd').csv" -Append -NoTypeInformation
+
+        }
+    }
     }
 }
