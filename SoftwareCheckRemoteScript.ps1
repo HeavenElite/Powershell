d9a3ba8a1a3d8cce7183a95e2e47a4ae6b4ecbaa
HeavenElite <heaven.elite@gmail.com>
2022-10-19 10:33:13 +0800

    Updated

--- SoftwareCheckRemoteScript.ps1
+++ SoftwareCheckRemoteScript.ps1
@@ -7,17 +7,21 @@ if ($Env:PROCESSOR_ARCHITECTURE -match '86') {
 
     if ($null -ne $Response) {
 
-        $Result = @{
-            Computer     = $IPAddress
-            Processor    = $Env:PROCESSOR_ARCHITECTURE
-            OSName       = [System.Environment]::OSVersion.VersionString
-            OSArck       = (wmic os get osarchitecture)[2]
-            Software     = $Response.DisplayName
-            SoftArck     = '32-Bit'
-            Version      = $Response.DisplayVersion
+        for ($i=0; $i -lt $Response.Length, $i++) {
+
+            $Result = @{
+
+                Computer     = $IPAddress
+                Processor    = $Env:PROCESSOR_ARCHITECTURE
+                OSName       = [System.Environment]::OSVersion.VersionString
+                OSArck       = (wmic os get osarchitecture)[2]
+                Software     = $Response[$i].DisplayName
+                SoftArck     = '32-Bit'
+                Version      = $Response[$i].DisplayVersion
+            }
+
+            [PSCustomObject]$Result
         }
-        
-        [PSCustomObject]$Result
     }
 
     else {
@@ -41,17 +45,21 @@ elseif ($Env:PROCESSOR_ARCHITECTURE -match '64') {
     
     if ($null -ne $Response) {
 
-        $Result = @{
-            Computer     = $IPAddress
-            Processor    = $Env:PROCESSOR_ARCHITECTURE
-            OSName       = [System.Environment]::OSVersion.VersionString
-            OSArck       = (wmic os get osarchitecture)[2]
-            Software     = $Response.DisplayName
-            SoftArck     = '64-Bit'
-            Version      = $Response.DisplayVersion
-        }
+        for ($i=0; $i -lt $Response.Length, $i++) {
 
-        [PSCustomObject]$Result
+            $Result = @{
+
+                Computer     = $IPAddress
+                Processor    = $Env:PROCESSOR_ARCHITECTURE
+                OSName       = [System.Environment]::OSVersion.VersionString
+                OSArck       = (wmic os get osarchitecture)[2]
+                Software     = $Response[$i].DisplayName
+                SoftArck     = '64-Bit'
+                Version      = $Response[$i].DisplayVersion
+            }
+
+            [PSCustomObject]$Result
+        }
     }
 
     else {
@@ -60,17 +68,21 @@ elseif ($Env:PROCESSOR_ARCHITECTURE -match '64') {
 
         if ($null -ne $Response) {
 
-            $Result = @{
-                Computer     = $IPAddress
-                Processor    = $Env:PROCESSOR_ARCHITECTURE
-                OSName       = [System.Environment]::OSVersion.VersionString
-                OSArck       = (wmic os get osarchitecture)[2]
-                Software     = $Response.DisplayName
-                SoftArck     = '32-Bit'
-                Version      = $Response.DisplayVersion
+            for ($i=0; $i -lt $Response.Length, $i++) {
+
+                $Result = @{
+    
+                    Computer     = $IPAddress
+                    Processor    = $Env:PROCESSOR_ARCHITECTURE
+                    OSName       = [System.Environment]::OSVersion.VersionString
+                    OSArck       = (wmic os get osarchitecture)[2]
+                    Software     = $Response[$i].DisplayName
+                    SoftArck     = '32-Bit'
+                    Version      = $Response[$i].DisplayVersion
+                }
+    
+                [PSCustomObject]$Result
             }
-            
-            [PSCustomObject]$Result
         }
 
         else {
