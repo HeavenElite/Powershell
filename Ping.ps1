73d9d7d75b41f6f582b6e7ef849a6bf8d3383a1d
HeavenElite <heaven.elite@gmail.com>
2022-10-24 17:06:27 +0800

    Updated

--- Ping.ps1
+++ Ping.ps1
@@ -1,9 +1,10 @@
-﻿$Servers = Import-Csv -Path .\ITLab\ITLabPing.csv -Header 'Protocal', 'IP', 'Port'
+﻿$Servers = Import-Csv -Path .\ITLab\ITLabPing.csv -Header 'Protocal', 'IP', 'Port', 'Site'
 
 for ( $i = 0; $i -lt $Servers.Length; $i++)
 {   
     $Report = @{
        No      =  $i + 1
+       Site    =  $Servers[$i].Site
        Server  =  $Servers[$i].IP
        Service =  $Servers[$i].Protocal
        Port    =  $Servers[$i].Port
@@ -20,8 +21,10 @@ for ( $i = 0; $i -lt $Servers.Length; $i++)
         $Report.Result = $false
     }
     
-    [PSCustomObject]$Report | Select-Object -Property No,Server,Service,Port,Result | Format-Table -AutoSize
-    [PSCustomObject]$Report | Select-Object -Property No,Server,Service,Port,Result | Export-Csv -Path ".\PingReport-$(Get-Date -Format "yyyy.MM.dd").csv" -NoTypeInformation -Append
+    [PSCustomObject]$Report | Select-Object -Property No,Site,Server,Service,Port,Result | Format-Table -AutoSize
+    [PSCustomObject]$Report | Select-Object -Property No,Site,Server,Service,Port,Result | Export-Csv -Path ".\PingReport-$(Get-Date -Format "yyyy.MM.dd").csv" -NoTypeInformation -Append
 }
+
+Import-Csv -Path ".\PingReport-$(Get-Date -Format "yyyy.MM.dd").csv" | Sort-Object -Property Result,Site | Format-Table -AutoSize
 
 [System.Console]::Beep(1000,1000)
