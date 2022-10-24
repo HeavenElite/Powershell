b7553db99d325b1c58868753612db398bf24e071
HeavenElite <heaven.elite@gmail.com>
2022-10-24 23:45:08 +0800

    Updated

--- Touch.ps1
+++ Touch.ps1
@@ -383,6 +383,3 @@ $Username   = Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.IP -eq "
 $Password   = ConvertTo-SecureString -AsPlainText -Force (Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.IP -eq "$IPAddress"} | Select-Object -ExpandProperty Password)
 $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
-UserCheck
-#$Response = Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {quser.exe}
-#$Response
