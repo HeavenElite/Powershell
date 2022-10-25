e12aedc4ebf94744d773df464f3bcd5e64e23e78
HeavenElite <heaven.elite@gmail.com>
2022-10-25 23:25:41 +0800

    Updated

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -6,6 +6,15 @@ for ( $i = 0; $i -lt ($Computers | Measure-Object).Count; $i++) {
     $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
     Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Stop-Computer -Force}
+
+    $Report = @{
+
+        IPAddress = $IPAddress
+        Shutdown  = CommandSent
+    }
+
+    $Report | Select-Object -Property IPAddress,Shutdown | Format-Table -AutoSize
+    $Report | Select-Object -Property IPAddress,Shutdown | Format-Table -AutoSize | Export-Csv -Path ".\ShutdownReport-$(Get-Date -Format 'yyyy.MM.dd').csv"
 }
 
 [System.Console]::Beep(1000,1000)
