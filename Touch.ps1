8835beed9068afe68113c0128a1572e18c28e2d3
HeavenElite <heaven.elite@gmail.com>
2022-10-27 14:13:48 +0800

    Updated

--- Touch.ps1
+++ Touch.ps1
@@ -398,8 +398,9 @@ function ResetPassword {
 # UserCheck
 # Logoff -ID
 
-$IPAddress  = '192.168.0.92'
+$IPAddress  = '192.168.1.10'
 $Username   = Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.IP -eq "$IPAddress"} | Select-Object -ExpandProperty Account
 $Password   = ConvertTo-SecureString -AsPlainText -Force (Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.IP -eq "$IPAddress"} | Select-Object -ExpandProperty Password)
 $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
+UserCheck
