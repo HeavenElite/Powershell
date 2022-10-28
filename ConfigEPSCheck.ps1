08b8592e4ddffdcfff353cb0b72069b03c51967c
HeavenElite <heaven.elite@gmail.com>
2022-10-28 18:13:34 +0800

    Updated

---
+++ ConfigEPSCheck.ps1
@@ -0,0 +1,17 @@
+$Computers = Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.EPS -eq 'EPS'}
+$Path      = ".\ConfigEPSCheck-$(Get-Date -Format 'yyyy.MM.dd').csv"
+
+for ($i=0; $i -lt ($Computers | Measure-Object).Count; $i++) {
+
+    $Username    = $Computers[$i].Account
+    $Password    = ConvertTo-SecureString -AsPlainText -Force $Computers[$i].Password
+    $Credential  = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
+    
+    $Response = Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -FilePath .\ConfigEPSCheckRemoteScript.ps1
+    $Response | Select-Object -Property ForwardServerIP,ForwardServerPort,FuelServerIP,FuelServerPort,SiteID,RPOSPort | Format-Table -AutoSize
+    $Response | Select-Object -Property ForwardServerIP,ForwardServerPort,FuelServerIP,FuelServerPort,SiteID,RPOSPort | Export-Csv -Path $Path -Append -NoTypeInformation
+}
+
+Import-Csv -Path $Path | Sort-Object -Property ForwardServerIP,ForwardServerPort,FuelServerIP,FuelServerPort,SiteID,RPOSPort | Format-Table -AutoSize
+
+[System.Console]::Beep(1000,1000)
