3aaf938a5bc29750b22948c76481abe8a27f40d9
HeavenElite <heaven.elite@gmail.com>
2022-10-31 19:48:22 +0800

    Updated

--- Touch.ps1
+++ Touch.ps1
@@ -443,7 +443,7 @@ function ConfigEPSCheck {
 # ConfigWinSCPCheck
 
 
-$IPAddress   = '192.168..132'
+$IPAddress   = '192.168.0.31'
 $Computer    = Import-Csv -Path .\ITLab\ITLabData.csv | Where-Object {$_.IP -eq $IPAddress}
 
 $Environment = $Computer.Test
@@ -454,4 +454,4 @@ $Username    = $Computer.Account
 $Password    = ConvertTo-SecureString -AsPlainText -Force $Computer.Password
 $Credential  = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
-UserCheck
+ConfigEPSCheck
