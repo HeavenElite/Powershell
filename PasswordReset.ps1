7261c68bf91b03e319b72a12f7bd8dd5416bb004
HeavenElite <heaven.elite@gmail.com>
2022-11-01 16:51:25 +0800

    Updated

---
+++ PasswordReset.ps1
@@ -0,0 +1,17 @@
+$Computers = Import-Csv -Path .\ITLab\ITLabDebug.csv
+$Count     = ($Computers | Measure-Object).Count
+
+$NewPassword = ConvertTo-SecureString -AsPlainText -Force 'Laurence'
+
+for ($i=0; $i -lt $Count; $i++) {
+
+    $Username    = $Computers[$i].Account
+    $OriPassword = ConvertTo-SecureString -AsPlainText -Force $Computers[$i].Password
+    $Credential  = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$OriPassword
+
+
+    Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Set-LocalUser -Name $using:Username -Password $using:NewPassword}
+
+
+
+}
