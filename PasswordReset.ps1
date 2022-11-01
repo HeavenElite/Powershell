9cc5e3d6a566b21e4ef714244910c66e8ce1cdd6
HeavenElite <heaven.elite@gmail.com>
2022-11-01 17:10:37 +0800

    Updated

--- PasswordReset.ps1
+++ PasswordReset.ps1
@@ -1,14 +1,51 @@
 $Computers = Import-Csv -Path .\ITLab\ITLabDebug.csv
 $Count     = ($Computers | Measure-Object).Count
 
-$NewPassword = ConvertTo-SecureString -AsPlainText -Force 'Laurence'
+$Path        = "PasswordResetReport-$(Get-Date -Format "yyyy.MM.dd").csv"
+$NewPassword = ConvertTo-SecureString -AsPlainText 'Laurence' -Force
 
 for ($i=0; $i -lt $Count; $i++) {
 
     $Username    = $Computers[$i].Account
-    $OriPassword = ConvertTo-SecureString -AsPlainText -Force $Computers[$i].Password
+    $OriPassword = ConvertTo-SecureString -AsPlainText $Computers[$i].Password -Force
     $Credential  = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$OriPassword
 
+    $LoopContinue = $true
 
-    Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Set-LocalUser -Name $using:Username -Password $using:NewPassword}
+    try {
+        Invoke-Command -ComputerName $Computers[$i].IP -Credential $Credential -ScriptBlock {Set-LocalUser -Name $using:Username -Password $using:NewPassword} -ErrorAction Stop
+    }
+
+    catch {
+        $Report = @{
+            Environment   = $Computers[$i].Test
+            SiteID        = $Computers[$i].Site
+            DeviceType    = $Computers[$i].Type
+            IPAddress     = $Computers[$i].IP
+            Operation     = 'ComputerOffline'
+        }
+        [PSCustomObject]$Report | Select-Object -Property Environment,SiteID,DeviceType,IPAddress,Operation | Format-Table -AutoSize
+        [PSCustomObject]$Report | Select-Object -Property Environment,SiteID,DeviceType,IPAddress,Operation | Export-Csv -Path $Path -Append -NoTypeInformation
+
+        $LoopContinue = $false
+    }
+
+    finally {
+    if ($LoopContinue) {
+
+        $Report = @{
+            Environment   = $Computers[$i].Test
+            SiteID        = $Computers[$i].Site
+            DeviceType    = $Computers[$i].Type
+            IPAddress     = $Computers[$i].IP
+            Operation     = 'ComputerOffline'
+        }
+        [PSCustomObject]$Report | Select-Object -Property Environment,SiteID,DeviceType,IPAddress,Operation | Format-Table -AutoSize
+        [PSCustomObject]$Report | Select-Object -Property Environment,SiteID,DeviceType,IPAddress,Operation | Export-Csv -Path $Path -Append -NoTypeInformation
+    }
+    }
 }
+
+Import-Csv -Path $Path | Sort-Object -Property Operation,Environment,SiteID,DeviceType | Format-Table -AutoSize
+
+[System.Console]::Beep(1000,1000)
