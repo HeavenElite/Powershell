c18c3e21d94ef2c8e38501f8a54084d1aeab0e56
HeavenElite <heaven.elite@gmail.com>
2022-11-10 12:17:39 +0800

    Updated

--- Touch.ps1
+++ Touch.ps1
@@ -464,6 +464,19 @@ function VMCheck {
 }
 
 
+# Time
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {w32tm /config /manualpeerlist:"ntp.ntsc.ac.cn time.windows.com" /syncfromflags:manual /update}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {w32tm /query /source}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {W32tm /resync /force}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {W32tm /register}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Start-Service W32time}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {sc.exe config W32time start= auto}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {(sc.exe qc w32time | Select-String -Pattern 'START_TYPE.*' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value).Split(':')[1]}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Get-Date -Format "yyyy.MM.dd-HH:mm"}
+# $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Get-Service W32time | Select-Object -ExpandProperty Status}
+# $Result
+
+
 # FunctionList
 # GetStorage
 # ShowBackup
@@ -500,16 +513,4 @@ $Password    = ConvertTo-SecureString -AsPlainText -Force $Computer.Password
 $Credential  = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
 
 
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {w32tm /config /manualpeerlist:"ntp.ntsc.ac.cn time.windows.com" /syncfromflags:manual /update}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {w32tm /query /source}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {W32tm /resync /force}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {W32tm /register}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Start-Service W32time}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {sc.exe config W32time start= auto}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {(sc.exe qc w32time | Select-String -Pattern 'START_TYPE.*' | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value).Split(':')[1]}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Get-Date -Format "yyyy.MM.dd-HH:mm"}
-#   $Result =  Invoke-Command -ComputerName $IPAddress -Credential $Credential -ScriptBlock {Get-Service W32time | Select-Object -ExpandProperty Status}
-
-    $Result
-
-#   UserCheck
+UserCheck
